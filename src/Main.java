2631dbbd024674da519c6e860c91aa4286647dd7
n10sit <johnmasters43@gmail.com>
2015-04-21 17:00:25 -0500

    skip and go back

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,8 @@ import javax.swing.border.*;
 public class Main extends JFrame {
 
 	private JPanel contentPane;
+	private final JButton btnNewButton_1 = new JButton("|<<");
+	private final JButton btnNewButton_2 = new JButton(">>|");
 
 	/**
 	 * Launch the application.
@@ -34,8 +36,16 @@ public class Main extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-	}
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Play");
+		btnNewButton.setBounds(190, 25, 55, 25);
+		contentPane.add(btnNewButton);
+		btnNewButton_1.setBounds(100, 25, 75, 25);
+		contentPane.add(btnNewButton_1);
+		btnNewButton_2.setBounds(257, 25, 75, 25);
+		contentPane.add(btnNewButton_2);
 
+	}
 }
